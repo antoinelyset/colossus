316e2b10eb43afc5fbc583c418be07223cfe46a0
antoinelyset <antoinelyset+github@gmail.com>
2014-12-01 16:03:43 +0100

    Add a timeout on push message

--- lib/colossus/writer_client.rb
+++ lib/colossus/writer_client.rb
@@ -14,7 +14,7 @@ class Colossus
       user_ids = Array(optional_user_ids) if optional_user_ids
       unique_token = generate_unique_token
       EM.synchrony do
-        EM::Synchrony.add_timer(time_out) { raise "Presence request timed out" }
+        EM::Synchrony.add_timer(time_out) { raise 'Presence request timed out' }
         EM::Synchrony.sync(faye_client.subscribe("/presences/response/#{unique_token}") { |message| return message['statuses'] })
         EM::Synchrony.sync(faye_client.publish("/presences/request/#{unique_token}", user_ids))
       end
@@ -24,7 +24,7 @@ class Colossus
       user_ids = Array(optional_user_ids) if optional_user_ids
       # Custom Content-Type to indicate to the server
       # this is a simple http request
-      conn     = Faraday.new(url, headers: {'Content-Type' => 'application/vnd.http.json'}) do |conf|
+      conn = Faraday.new(url, headers: {'Content-Type' => 'application/vnd.http.json'}) do |conf|
         conf.adapter  Faraday.default_adapter
         conf.response :raise_error
       end
@@ -40,6 +40,7 @@ class Colossus
     def push_message(user_ids, message)
       user_ids = Array(user_ids)
       EM.synchrony do
+        EM::Synchrony.add_timer(time_out) { raise 'Push message timed out' }
         user_ids.each do |user_id|
           EM::Synchrony.sync(faye_client.publish("/users/#{user_id}", message))
         end
@@ -50,7 +51,7 @@ class Colossus
     private
 
     def faye_client
-      faye_client  = ::Faye::Client.new(url)
+      faye_client = ::Faye::Client.new(url)
       faye_client.add_extension(FayeExtension.new(writer_token))
       faye_client
     end
